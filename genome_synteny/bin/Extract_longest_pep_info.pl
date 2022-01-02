efefb8d3a34cd735017ff03831ae8e9519aa333f
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2022-01-02 19:25:58 +0800

    Create Extract_longest_pep_info.pl

---
+++ genome_synteny/bin/Extract_longest_pep_info.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my ($gene, $tran); my %seq; my %hash; my @genes;
+&Parse_geno($ARGV[1]);
+
+my %info;
+foreach my $gene (@genes) {
+	my @trans=sort @{$hash{$gene}};
+	foreach my $tran (@trans) {
+		my $seq=$seq{$gene}->{$tran};
+		my $len=length($seq);
+		if ($info{$gene}) {
+			my $len_old=$info{$gene}->{'len'};
+			if ($len > $len_old) {
+				$info{$gene}={
+					'tran' => $tran,
+					'len'  => $len
+				};
+			}
+		} else {
+			$info{$gene}={
+				'tran' => $tran,
+				'len'  => $len
+			};
+		}
+	}
+}
+
+foreach my $gene (@genes) {
+	my $tran=$info{$gene}->{'tran'};
+	my $len =$info{$gene}->{'len'};
+	print "$gene\t$tran\t$len\n";
+}
+
+sub Parse_geno {
+	my $phase=$ARGV[1];
+	open GENOME, $ARGV[0] or die "can not open $ARGV[0]\n";
+	if ($phase==1) {
+		while (<GENOME>) {
+		chomp;
+		if (/^>(.*)\|(.*)/) {
+			($gene, $tran)=($1, $2);
+			$hash{$gene}=[] unless exists $hash{$gene};
+			push @{$hash{$gene}}, $tran;
+			push @genes, $gene;
+			} else {
+				$seq{$gene}->{$tran}.=$_;
+			}
+		}
+	} elsif ($phase==0) {
+		while (<GENOME>) {
+		chomp;
+		if (/^>(.*)\./) {
+			$gene=$1; s/>//; $tran=$_;
+			$hash{$gene}=[] unless exists $hash{$gene};
+			push @{$hash{$gene}}, $tran;
+			push @genes, $gene;
+			} else {
+				$seq{$gene}->{$tran}.=$_;
+			}
+		}
+	}
+}
