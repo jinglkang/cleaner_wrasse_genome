19ecfac300ea8195973b32dafe6b1059d47829ca
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2022-03-23 11:21:01 +0800

    Create Estimate_F_index_plot.pl

---
+++ Enrichment_analysis/Estimate_F_index_plot.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+my $fm="summary_run2_anc.txt";
+my $a1=(12**2-2*12)/2;
+my $b1=(log2($a1+1/2))+1;
+my $header;
+my %header;
+my @nums;
+
+open FM, $fm or die "can not open $fm\n";
+while (<FM>) {
+        chomp;
+        my @a=split /\t/;
+        my $total;
+        if (/^Family/) {
+                $header="Family_ID\t";
+                for (my $i = 1; $i < @a; $i++) {
+                        if ($a[$i]=~/\D+\<\d+\>/) {
+                                (my $spe)=$a[$i]=~/(\D+)\<\d+\>/;
+                                $header.=$spe."\t";
+                                $header{$i}=$spe;
+                                push @nums, $i;
+                        }
+                }
+                $header=~s/\s+$//;
+#                print "$header\n";
+        } else {
+                foreach my $nb (@nums) {
+                        $total+=$a[$nb];
+                }
+                my $F_T="Family_".$a[0]."\t";
+                foreach my $nb (@nums) {
+                        my $ratio=$a1*($a[$nb]/$total);
+                        my $c1=(log2($ratio+1/2))+1;
+                        my $d1=$c1/$b1;
+                        $d1=sprintf("%.3f",$d1);
+#                        $F_T.=$d1."\t";
+                        print "$F_T\t$header{$nb}\t$d1\n";
+                }
+#                $F_T=~s/\s+$//;
+#                print "$F_T\n";
+        }
+}
+
+sub log2 {
+        my $n = shift;
+        # using pre-defined log function
+        return log($n) / log(2);
+}
