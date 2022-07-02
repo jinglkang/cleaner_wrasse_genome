f0d4af455cba9fc4956d9847db3b6b98b49a056c
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2022-07-02 22:43:13 +0800

    Update run_Gmfm_detect.pl

--- Gene_family_4/Spec_fm_detection/run_Gmfm_detect.pl
+++ Gene_family_4/Spec_fm_detection/run_Gmfm_detect.pl
@@ -6,10 +6,10 @@ my @species=qw(Cheilinus_undulatus Fugu Labroides_dimidiatus Labrus_bergylta Med
 my $swiss="~/Desktop/Annotation_database/swiss-prot/uniprot-filtered-reviewed_yes.fasta";
 my $query=$ARGV[0];
 foreach my $spe (@species) {
-        my $dir  ="/media/HDD/cleaner_fish/genome/All_genomes/";
+        my $dir  ="/media/HDD/cleaner_fish/genome/All_genomes";
         my $fasta="$spe.fasta";
         my $genom="$dir/$fasta";
-        my $gtf  ="$dir/$spe.fasta";
+        my $gtf  ="$dir/$spe.gtf";
         my $pep  ="$dir/longest_pep/$fasta";
         system("perl Gmfm_detect --genome $genom --species $spe --intron 1000 --query $ARGV[0] --uniprot $swiss --gtf $gtf --pep $pep");
 }
